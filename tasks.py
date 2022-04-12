7926647c6c08219f12f6738f30f55283e5ce89ab
xinetzone <xinzone@outlook.com>
2022-04-13 04:04:57 +0800

    modified:   tasks.py

--- tasks.py
+++ tasks.py
@@ -1,10 +1,12 @@
 from invoke import task
 from d2py.tools.write import site
 
+
 @task
 def init(ctx):
-    ctx.run('pip install .[doc]')
-    ctx.run('pip install --upgrade nbconvert xarray')
+    ctx.run("pip install .[doc]")
+    ctx.run("pip install --upgrade nbconvert xarray")
+
 
 namespace = site('docs', target='output/html')
 namespace.add_task(init)
